40f9539d1fadaa40f5faedce4afabd8a3862dd25
Zayden <zaylanthier@gmail.com>
2026-03-04 12:35:22 -0500

    finished Code

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,21 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+
+if (temperature > 80){
+    console.log("Watering on")
+}else{
+    console.log("Watering off")
+}
+
+if (timeOfDay === "evening", "night") {
+    console.log("Lights on")
+} else {
+    console.log("Lights off")
+}
+
+while (soilMoisture < 40) {
+    soilMoisture = soilMoisture + 5
+    console.log(soilMoisture)
+}
